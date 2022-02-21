09a105e7278d060dca8b842738b34e8ca0bb6fa9
unknown <gelargew@gmail.com>
2022-02-21 17:03:27 +0700

    test

--- hello.py
+++ hello.py
@@ -9,3 +9,5 @@ ls = ','.join([country for country in countries])
 url = f'https://disease.sh/v3/covid-19/historical/{ls}?lastdays=360'
 data = requests.get(url)
 data = json.loads(data.text)
+with open('countries_test.json', 'w') as f:
+    json.dump(data, f, indent=4)
