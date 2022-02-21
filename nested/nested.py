f20942e4fa7db1c98585f42f5447801d83b8d6b9
unknown <gelargew@gmail.com>
2022-02-21 19:30:05 +0700

    test

--- nested/nested.py
+++ nested/nested.py
@@ -1,4 +1,9 @@
 from os import walk
+from pathlib import Path
+import os
 
-with open('countries.json', 'r') as f:
+BASE_DIR = Path(__file__).resolve().parent.parent
+path = os.path.join(BASE_DIR, 'countries.json')
+
+with open(path, 'r') as f:
     print(f)
