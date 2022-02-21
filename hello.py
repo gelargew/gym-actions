87cfb01b9587fb8661e61ebc94171873a1e5471c
unknown <gelargew@gmail.com>
2022-02-21 17:07:13 +0700

    test

--- hello.py
+++ hello.py
@@ -6,8 +6,8 @@ with open('countries.json', 'r') as f:
     countries = json.load(f)
 
 ls = ','.join([country for country in countries])
-url = f'https://disease.sh/v3/covid-19/historical/{ls}?lastdays=360'
+""" url = f'https://disease.sh/v3/covid-19/historical/{ls}?lastdays=360'
 data = requests.get(url)
-data = json.loads(data.text)
+data = json.loads(data.text) """
 with open('countries_test.json', 'w') as f:
-    json.dump(data, f, indent=4)
+    json.dump(countries, f, indent=4)
